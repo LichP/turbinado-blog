c24786de09038d1a18067f96191ec73dac49ffa4
Phil Stewart <p.stewart@imperial.ac.uk>
2024-01-27 00:39:01 +0000

    app/page: Cope with null posts for production build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 async function getPostsByDateDesc() {
     const posts = await getPosts()
-    return posts.sort((a, b) => b.date.getTime() - a.date.getTime())
+    return posts.sort((a, b) => (b?.date.getTime() ?? 0) - (a?.date.getTime() || 0))
 }
 
 export default async function Home() {
@@ -38,19 +38,24 @@ export default async function Home() {
                 <h2>Blogs</h2>
                 <ul className="list-none space-y-4 px-0">
                     {posts.map((post) => {
-                        const publishedDate = post.date.toLocaleDateString('en-GB', {
-                            month: 'long',
-                            day: 'numeric',
-                            year: 'numeric',
-                        })
+                        if (post) {
+                            const publishedDate = post.date.toLocaleDateString('en-GB', {
+                                month: 'long',
+                                day: 'numeric',
+                                year: 'numeric',
+                            })
 
-                        return (
-                            <li key={post.slug}>
-                                <p className="font-bvs text-zinc-750 dark:text-zinc-400">{publishedDate}</p>
-                                <h3><Link href={`/post/${post.slug}`}>{post.title}</Link></h3>
-                                <p>{post.description}</p>
-                            </li>
-                        )
+                            return (
+                                <li key={post.slug}>
+                                    <p className="font-bvs text-zinc-750 dark:text-zinc-400">{publishedDate}</p>
+                                    <h3><Link href={`/post/${post.slug}`}>{post.title}</Link></h3>
+                                    <p>{post.description}</p>
+                                </li>
+                            )
+                        }
+                        else {
+                            return null;
+                        }
                     })}
                 </ul>
             </section>
