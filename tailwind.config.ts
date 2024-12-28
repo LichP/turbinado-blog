d0ac32f967d50e2286db02d0d364bb8669a1cc4a
Phil Stewart <p.stewart@imperial.ac.uk>
2024-12-28 19:52:09 +0000

    Tailwind: Add sugar palette to colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,22 @@ const config: Config = {
   ],
   theme: {    
     extend: {
+      colors: {
+        'sugar': {
+          DEFAULT: '#E2E7B3',
+          50: '#F7FBDA',
+          100: '#EDF2C4',
+          200: '#E2E7B3',
+          300: '#D5D9A6',
+          400: '#CACD9F',
+          500: '#B9BB8B',
+          600: '#A2A470',
+          700: '#838458',
+          800: '#616142',
+          900: '#40402B',
+          950: '#2E2D1F'
+        },
+      },
       fontFamily: {
         'sans': ['var(--font-lato)', ...defaultTheme.fontFamily.sans],
         'bvs': ['var(--font-bitstream-vera-sans)', 'var(--font-lato)', ...defaultTheme.fontFamily.sans],
